b47f4b3387dca9573e51f355a8159cb7aea3668e
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-03-08 11:17:20 +0100

    small fixes

--- MIMUW_2016/materialy/lab2.R
+++ MIMUW_2016/materialy/lab2.R
@@ -1,23 +1,23 @@
 # Lab 2 - Statistics 2
-
+ 
 ## Simulations
-
-Generate two-dimensional vector of random variables with corelation rho.
-
+ 
+Generate two-dimensional vector of random variables with correlation rho.
+ 
 Try Pearson test / Spearman test on this pair.
-
+ 
 Calculate power for both tests as a function of sample size.
-
+ 
 Then try to use non gaussian distribution (log-gaussian, cauchy) and compare power for both tests.
-
-
+ 
+ 
 ## Exploration
-
-- Find if there is any relation forauta2012 dataset between Color and Brand
-
+ 
+- Find out if there is any relation forauta2012 dataset between Color and Brand.
+ 
 Which test, which method should be used?
-
-- Find if there is any relation between Price and Milage
-
+ 
+- Find out if there is any relation between Price and Mileage.
+ 
 How to plot it? How to verify it?
-  
+
