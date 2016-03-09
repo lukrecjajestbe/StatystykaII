28d20f5d28b2de5c9f3431b3cdd797fdd97a78bc
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-03-09 11:47:50 +0100

    more detailed instructions

--- MIMUW_2016/materialy/lab2.R
+++ MIMUW_2016/materialy/lab2.R
@@ -2,18 +2,34 @@
  
 ## Simulations
  
-Generate two-dimensional vector of random variables with correlation rho.
- 
-Try Pearson test / Spearman test on this pair.
- 
-Calculate power for both tests as a function of sample size.
- 
-Then try to use non gaussian distribution (log-gaussian, cauchy) and compare power for both tests.
- 
- 
+1. Generate two independent samples from gaussian distribution (100 observations each)
+check: rnorm
+
+2. Compute Pearson correlation between these two vectors
+check: cor
+
+3. Replicate this setting 10000 times and collect correlations from different runs
+check: replicate
+
+4. Present distribution of cor coefficients
+check: hist
+
+5. Find critical regions fot this test
+check: quantile
+
+6. Generate two samples that are correlated, calculate cor coefficients
+
+7. Calculate power for this setting: fraction of samples that fall in the critical region
+
+8. Calculate power as a function of real correlation. How the power behaves? Is it along our expectations?
+
+9. Calculate power as a function of sample size
+
+10. Try non gaussian distributions (log-gaussian, cauchy)  
+
 ## Exploration
  
-- Find out if there is any relation forauta2012 dataset between Color and Brand.
+- Find out if there is any relation in auta2012 dataset between Color and Brand.
  
 Which test, which method should be used?
  
